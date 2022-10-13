e023c94896a4a8c2b642f09a2559174f6ca139d5
Valentin <valiklolanew@gmail.com>
2022-10-13 18:49:45 +0300

    added project 2

---
+++ Project_02/Models/MainModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_02.Models
+{
+    internal class MainModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        private bool _isLogin { get; set; }
+        public bool IsLogin
+        {
+            get { return _isLogin; }
+            set
+            {
+                _isLogin = value;
+                OnPropertyChanged("IsLogin");
+            }
+        }
+    }
+}
---
+++ Project_02/Views/MainView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Project_02.ViewModels;
+using System.Windows.Controls;
+
+namespace Project_02.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+    }
+}

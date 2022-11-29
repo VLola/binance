6e12f0011543596ba732a1343e3ad437a70c504b
VLola <valiklolanew@gmail.com>
2022-11-29 08:35:05 +0200

    fixed bugs

--- Project_06/ViewModels/MainViewModel.cs
+++ Project_06/ViewModels/MainViewModel.cs
@@ -82,7 +82,7 @@ namespace Project_06.ViewModels
         }
         private void StartNewAlgo(SymbolModel symbolModel)
         {
-            List<IBinanceKline> binanceKlines = Klines(symbolModel.Name, KlineInterval.FifteenMinutes, 400);
+            List<IBinanceKline> binanceKlines = Klines(symbolModel.Name, KlineInterval.OneMinute, 400);
 
             List<OHLC> oHLCs = binanceKlines.Select(item => new OHLC(
                     open: Decimal.ToDouble(item.OpenPrice),
@@ -96,7 +96,7 @@ namespace Project_06.ViewModels
 
             List<double> x = new();
             List<double> y = new();
-            int mul = 4;
+            int mul = 10;
 
             symbolModel.Plus = 0;
             symbolModel.PlusPercent = 0;
@@ -145,6 +145,7 @@ namespace Project_06.ViewModels
                                 symbolModel.MinusPercent += Math.Round((oHLCs[i + 1].Close - oHLCs[i + 2].Close) / oHLCs[i + 2].Close * 10000);
                             }
                         }
+
                     }
                 }
             }
@@ -269,7 +270,8 @@ namespace Project_06.ViewModels
         {
             try
             {
-                var result = _client.UsdFuturesApi.ExchangeData.GetKlinesAsync(symbol: symbol, interval: interval, limit: limit, startTime: DateTime.UtcNow.AddDays(-10)).Result;
+                //var result = _client.UsdFuturesApi.ExchangeData.GetKlinesAsync(symbol: symbol, interval: interval, limit: limit, startTime: DateTime.UtcNow.AddDays(-30)).Result;
+                var result = _client.UsdFuturesApi.ExchangeData.GetKlinesAsync(symbol: symbol, interval: interval, limit: limit).Result;
                 if (!result.Success) WriteLog("Error Candles");
                 else
                 {

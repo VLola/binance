38b5842bda45ccc59e675319192da5cd6f456e46
VLola <valiklolanew@gmail.com>
2023-01-06 10:52:45 +0200

    aded algo

--- Project_07/Models/SymbolModel.cs
+++ Project_07/Models/SymbolModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Project_07.Models
+﻿using System;
+
+namespace Project_07.Models
 {
     public class SymbolModel : ChangedModel
     {
@@ -10,6 +12,117 @@
             {
                 _name = value;
                 OnPropertyChanged("Name");
+            }
+        }
+        private decimal _price { get; set; }
+        public decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                _price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+        private DateTime _openTime { get; set; }
+        public DateTime OpenTime
+        {
+            get { return _openTime; }
+            set
+            {
+                _openTime = value;
+                OnPropertyChanged("OpenTime");
+            }
+        }
+        private decimal _priceShort { get; set; }
+        public decimal PriceShort
+        {
+            get { return _priceShort; }
+            set
+            {
+                _priceShort = value;
+                OnPropertyChanged("PriceShort");
+            }
+        }
+        private decimal _takeProfitShort { get; set; }
+        public decimal TakeProfitShort
+        {
+            get { return _takeProfitShort; }
+            set
+            {
+                _takeProfitShort = value;
+                OnPropertyChanged("TakeProfitShort");
+            }
+        }
+        private decimal _stopLossShort { get; set; }
+        public decimal StopLossShort
+        {
+            get { return _stopLossShort; }
+            set
+            {
+                _stopLossShort = value;
+                OnPropertyChanged("StopLossShort");
+            }
+        }
+        private decimal _priceLong { get; set; }
+        public decimal PriceLong
+        {
+            get { return _priceLong; }
+            set
+            {
+                _priceLong = value;
+                OnPropertyChanged("PriceLong");
+            }
+        }
+        private decimal _takeProfitLong { get; set; }
+        public decimal TakeProfitLong
+        {
+            get { return _takeProfitLong; }
+            set
+            {
+                _takeProfitLong = value;
+                OnPropertyChanged("TakeProfitLong");
+            }
+        }
+        private decimal _stopLossLong { get; set; }
+        public decimal StopLossLong
+        {
+            get { return _stopLossLong; }
+            set
+            {
+                _stopLossLong = value;
+                OnPropertyChanged("StopLossLong");
+            }
+        }
+
+        private bool _isOpenShort { get; set; }
+        public bool IsOpenShort
+        {
+            get { return _isOpenShort; }
+            set
+            {
+                _isOpenShort = value;
+                OnPropertyChanged("IsOpenShort");
+            }
+        }
+        private bool _isOpenLong { get; set; }
+        public bool IsOpenLong
+        {
+            get { return _isOpenLong; }
+            set
+            {
+                _isOpenLong = value;
+                OnPropertyChanged("IsOpenLong");
+            }
+        }
+        private double _total { get; set; }
+        public double Total
+        {
+            get { return _total; }
+            set
+            {
+                _total = value;
+                OnPropertyChanged("Total");
             }
         }
     }
--- Project_07/ViewModels/SymbolViewModel.cs
+++ Project_07/ViewModels/SymbolViewModel.cs
@@ -1,11 +1,17 @@
 ﻿using Binance.Net.Clients;
+using Binance.Net.Enums;
+using Binance.Net.Interfaces;
 using Binance.Net.Objects.Models.Futures;
 using Project_07.Models;
+using System;
+using System.IO;
+using System.Windows.Media;
 
 namespace Project_07.ViewModels
 {
     public class SymbolViewModel
     {
+        private string _pathLog = $"{Directory.GetCurrentDirectory()}/log/";
         public BinanceClient? Client { get; set; }
         public BinanceSocketClient? SocketClient { get; set; }
         public SymbolModel SymbolModel { get; set; } = new(); 
@@ -14,6 +20,112 @@ namespace Project_07.ViewModels
             SymbolModel.Name = symbol.Name;
             Client = client;
             SocketClient = socketClient;
+            SymbolModel.PropertyChanged += SymbolModel_PropertyChanged;
+            StartKlineAsync();
+        }
+
+        private void SymbolModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == "Price")
+            {
+                CheckOpenOrder();
+                CheckCloseOrder();
+            }
+        }
+        private void CheckOpenOrder()
+        {
+            if (!SymbolModel.IsOpenShort)
+            {
+                if (SymbolModel.Price < SymbolModel.PriceShort)
+                {
+                    SymbolModel.IsOpenShort = true;
+                }
+            }
+            if (!SymbolModel.IsOpenLong)
+            {
+                if (SymbolModel.Price > SymbolModel.PriceLong)
+                {
+                    SymbolModel.IsOpenLong = true;
+                }
+            }
+        }
+        private void CheckCloseOrder()
+        {
+            if (SymbolModel.IsOpenShort)
+            {
+                if (SymbolModel.Price < SymbolModel.TakeProfitShort)
+                {
+                    SymbolModel.Total += 0.5;
+                    SymbolModel.IsOpenShort = false;
+                }
+                else if (SymbolModel.Price > SymbolModel.StopLossShort)
+                {
+                    SymbolModel.Total -= 0.5;
+                    SymbolModel.IsOpenShort = false;
+                }
+            }
+            if (SymbolModel.IsOpenLong)
+            {
+                if (SymbolModel.Price > SymbolModel.TakeProfitLong)
+                {
+                    SymbolModel.Total += 0.5;
+                    SymbolModel.IsOpenLong = false;
+                }
+                else if (SymbolModel.Price < SymbolModel.StopLossLong)
+                {
+                    SymbolModel.Total -= 0.5;
+                    SymbolModel.IsOpenLong = false;
+                }
+            }
+        }
+
+        public async void StartKlineAsync()
+        {
+            try
+            {
+                var result = await SocketClient.UsdFuturesStreams.SubscribeToKlineUpdatesAsync(SymbolModel.Name, KlineInterval.OneMinute, Message =>
+                {
+                    SymbolModel.Price = Message.Data.Data.ClosePrice;
+                    if (Message.Data.Data.OpenTime == SymbolModel.OpenTime) UpdateKline(Message.Data.Data);
+                    else NewKline(Message.Data.Data);
+                });
+                if (!result.Success)
+                {
+                    WriteLog($"Failed StartKlineAsync: {result.Error?.Message}");
+                }
+            }
+            catch (Exception eX)
+            {
+                WriteLog($"StartKlineAsync {eX.Message}");
+            }
+        }
+        private void UpdateKline(IBinanceKline binanceKline)
+        {
+
+        }
+        private void NewKline(IBinanceKline binanceKline)
+        {
+            decimal open = binanceKline.OpenPrice;
+            if (!SymbolModel.IsOpenShort)
+            {
+                SymbolModel.PriceShort = open - (open * 0.01m);
+                SymbolModel.TakeProfitShort = open - (open * 0.015m);
+                SymbolModel.StopLossShort = open - (open * 0.005m);
+            }
+            if (!SymbolModel.IsOpenLong)
+            {
+                SymbolModel.PriceLong = open + (open * 0.01m);
+                SymbolModel.TakeProfitLong = open + (open * 0.015m);
+                SymbolModel.StopLossLong = open + (open * 0.005m);
+            }
+        }
+        private void WriteLog(string text)
+        {
+            try
+            {
+                File.AppendAllText(_pathLog + SymbolModel.Name, $"{DateTime.Now} {text}\n");
+            }
+            catch { }
         }
     }
 }
